c3f6f057066acfbe4312aee716e6abc2bf7c6a42
ASUS <anahita_fakhravar@yahoo.com>
2020-11-25 23:24:44 +0330

    Create login reducer

---
+++ ifs-chat/src/redux/reducers/LoginReducer.js
@@ -0,0 +1,23 @@
+//Save username action and reducer 
+
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = { username: '' }
+
+const SaveLoginInfoSlice = createSlice({
+
+    name: 'mySaveContact',
+    initialState,
+    reducers: {
+        saveLoginInfo(state) {
+            state.username = false;
+        },
+        saveLoginInfoSuccess(state, action) {
+            state.username = action.payload.username;
+        },
+    },
+});
+
+export const { saveLoginInfo ,saveLoginInfoSuccess} = SaveLoginInfoSlice.actions;
+
+export default SaveLoginInfoSlice.reducer;
