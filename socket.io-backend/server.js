fdcbc0ccf06094e98e17a69110cd40885dc93513
ASUS <anahita_fakhravar@yahoo.com>
2020-11-25 23:26:31 +0330

    Assign unique avator for each user

--- socket.io-backend/server.js
+++ socket.io-backend/server.js
@@ -5,6 +5,12 @@ const messageHandler = require('./handlers/message.handler')
 let currentUserId = 2;
 const users = {}
 
+function createUserAvatorUrl() {
+    const rand1 = Math.round(Math.random() * 200 + 100);
+    const rand2 = Math.round(Math.random() * 200 + 100);
+    return `https://placeimg.com/${rand1}/${rand2}/any`
+}
+
 io.on('connection', socket => {
     console.log('as user connected!')
     console.log(socket.id)
